0a0f6bcbbc2f7a6aa993d2df1015287003e2c948
Tahir Nasoordeen Packeer <142225944+tahir-packeer@users.noreply.github.com>
2024-11-07 14:05:30 +0300

    newwwww

--- vibrant-CRM-final/resources/views/categories/create.blade.php
+++ vibrant-CRM-final/resources/views/categories/create.blade.php
@@ -18,10 +18,6 @@
                 <label for="description" class="form-label text-white">Description</label>
                 <textarea name="description" class="form-control" id="description"></textarea>
             </div>
-            <div class="mb-3">
-                <label for="fabric_type" class="form-label text-white">Fabric Type</label>
-                <input type="text" name="fabric_type" class="form-control" id="fabric_type">
-            </div>
             <button type="submit" class="btn btn-primary">Create</button>
         </form>
     </div>
